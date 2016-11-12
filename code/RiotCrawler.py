c5797ce7d654d4dcbc302980ac8ef657f1cf5082
DavidCorn <qqin@andrew.cmu.edu>
2016-11-11 20:34:26 -0500

    change terminology to make it consistent with notebook

--- code/RiotCrawler.py
+++ code/RiotCrawler.py
@@ -9,7 +9,7 @@ class RiotCrawler:
     def __init__(self, key):
         self.key = key
         self.w = RiotWatcher(key)
-        self.division = {
+        self.tiers = {
             'bronze': [],
             'silver': [],
             'gold': [],
@@ -32,29 +32,25 @@ class RiotCrawler:
     #                     player_list.add(fellow_player_id)
     #     return list(player_list)
 
-    def get_player_by_division(self, summoner_id):
+    def get_player_by_tier(self, summoner_id):
         request_url = 'https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/{}?api_key={}'.format(
             summoner_id, self.key
         )
         response = urllib2.urlopen(request_url)
-        division_info = ujson.loads(response.read())
+        tier_info = ujson.loads(response.read())
 
-        tier = division_info[str(summoner_id)][0]['tier'].lower()
-        entries = division_info[str(summoner_id)][0]['entries']
+        tier = tier_info[str(summoner_id)][0]['tier'].lower()
+        entries = tier_info[str(summoner_id)][0]['entries']
 
-        level = self.division[tier]
-        # cnt = 0
+        level = self.tiers[tier]
         for entry in entries:
             level.append(entry['playerOrTeamId'])
-            # cnt += 1
-            # if cnt == 3:
-            #     break
 
-        # for l in level:
-        #     print 'summoner id: {}, name: {}'.format(str(l.id), l.name)
+        for l in level:
+            print 'summoner id: {}'.format(str(l))
 
 
-def get_division():
+def get_tier():
     # challenger: 77759242
     # platinum: 53381
     # gold: 70359816
@@ -66,13 +62,13 @@ def get_division():
     riot_crawler = RiotCrawler(config['key'])
     for player_id in player_ids:
         print 'start crawling id: {}'.format(player_id)
-        riot_crawler.get_player_by_division(player_id)
-    return riot_crawler.division
+        riot_crawler.get_player_by_tier(player_id)
+    return riot_crawler.tiers
 
-
-# for tier, rank_dict in riot_crawler.division.iteritems():
-#     print '--- {} ---'.format(tier)
-#     for summoner in rank_dict:
-#         print 'summoner id: {}'.format(summoner)
-#     print '--- end of {} ---'.format(tier)
+tiers = get_tier()
+for tier, rank_dict in tiers.iteritems():
+    print '--- {} ---'.format(tier)
+    for summoner in rank_dict:
+        print 'summoner id: {}'.format(summoner)
+    print '--- end of {} ---'.format(tier)
 
